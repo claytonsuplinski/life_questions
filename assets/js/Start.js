eaac6f399a7d1ae4ea02adf3659d85a9283d3e50
claytonsuplinski <claytonsuplinski@gmail.com>
2020-11-02 10:35:29 -0600

    Added answers to home view. More questions.

--- assets/js/Start.js
+++ assets/js/Start.js
@@ -5,7 +5,7 @@ QUE.start = function( callback ){
 		url      : './assets/data/questions.json',
 		dataType : 'json',
 		success  : function( data ){
-			QUE.data = data;
+			QUE.data = data.sort(function(a,b){ return ( a.answer ? -1 : 1 ); });
 			
 			if( callback ) callback();
 		},
--- assets/js/views/Home.js
+++ assets/js/views/Home.js
@@ -7,6 +7,9 @@ QUE.views.home.draw = function(){
 			QUE.data.map(function( question ){
 				return '<a class="question" href="' + QUE.hashlinks.get_url({ include : { question : question.question } }) + '">' +
 					'<div class="question-name">' + question.question + '</div>' +
+					( question.answer ? 
+						'<div class="question-answer">' + question.answer[ 0 ][ 1 ] + '</div>'
+					: '' ) +
 				'</a>';
 			}).join('') +
 		'</div>'
